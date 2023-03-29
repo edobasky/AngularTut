b8f5c7e07262e199c05788dab029fc78b99cbfbd
edobasky <edore7@gmail.com>
2023-03-29 04:59:35 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+
+import { AppComponent } from './app.component';
+import { productListComponent } from './products/product-list.component';
+import { ConvertToSpacePipe } from './shared/convert-to-spaces';
+import { StarComponent } from './shared/star.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    productListComponent,
+    ConvertToSpacePipe,
+    StarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { ProductService } from "./product.service";
+import { IProduct } from "./products";
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls : ['./product-list.component.css']
+})
+export class productListComponent implements OnInit,OnDestroy {
+
+  pageTitle : string = 'Product List';
+  imageWidth : number = 50;
+  imageMargin : number = 2;
+  showImage : boolean = false;
+  errormessage : string = '';
+  sub!: Subscription;
+
+
+  constructor(private productService : ProductService) {}
+
+
+  private _listFilter = '';
+
+  get listFilter() : string {
+    return this._listFilter;
+  }
+
+  set listFilter(value : string) {
+    this._listFilter = value;
+    console.log("In setter:", value);
+    this.filteredProducts =  this.performFilter(value);
+  }
+
+  filteredProducts : IProduct[] = [];
+  products: IProduct[] = [
+  ];
+
+
+
+  toggleImage() : void {
+    this.showImage = !this.showImage
+  }
+
+  ngOnInit(): void {
+   this.sub = this.productService.getProducts().subscribe({
+      next : products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error : err => this.errormessage = err
+    });
+
+  }
+
+  performFilter(filterBy: string) : IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+    product.productName.toLocaleLowerCase().includes(filterBy) )
+  }
+
+  onRatingClicked(message : string) : void {
+    this.pageTitle = 'product List: ' + message;
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+}
